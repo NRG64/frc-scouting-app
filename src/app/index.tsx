78311070f895053f526e398682a96350cbac0a75
NRG64 <ryanrhino64@gmail.com>
2025-01-22 15:15:03 +1100

    feat: added modal for button popup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,8 @@
+
+import { Stack } from "expo-router";
 import { Link } from "expo-router";
-import React from "react";
-import { Text, View , TouchableWithoutFeedback, TouchableOpacity, StyleSheet, SafeAreaView, Image, Button} from "react-native";
+import React, {useState} from "react";
+import { Text, View , TouchableWithoutFeedback, TouchableOpacity, StyleSheet, SafeAreaView, Image, Button, Modal} from "react-native";
 
 interface TouchAreaProps {
   imageSource: '../assets/gameField'
@@ -26,12 +28,19 @@ const buttonConfigs = [
 ];
 
 const index = () => {
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [activeButton, setActiveButton] = useState<string | null>(null); // track active button 
+
   const onPress = (buttonName: string): void => {
-    console.log(`Button ${buttonName} pressed!`);
+    setActiveButton(buttonName);
+    setModalVisible(true); 
   };
 
   return (
+
     <SafeAreaView style={styles.container}>
+
       <Image
         source={require("../assets/gameField.png")}
         style={{ width: 1210, height: 660 }}
@@ -49,6 +58,18 @@ const index = () => {
           />
         </View>
       ))}
+        <Modal
+        transparent={true}
+        visible={modalVisible}
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Button Pressed: {activeButton}</Text>
+            <Button title="Close Modal" onPress={() => setModalVisible(false)} />
+          </View>
+        </View>
+        </Modal>
+
     </SafeAreaView>
   );
 };
@@ -60,6 +81,29 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     position: "absolute", // Allows precise placement of buttons
+  },
+  link: {
+    paddingTop: 20,
+    fontSize: 20 ,
+    
+  },
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContent: {
+    width: 300,
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 20,
+    alignItems: "center",
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
   },
 });
 export default index
